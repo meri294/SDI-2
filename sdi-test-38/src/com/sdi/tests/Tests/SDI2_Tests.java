dde26022f89984274be0f614785496e2a74822d1
meri294 <merigg94@gmail.com>
2016-03-29 13:15:10 +0200

    Test de inicio, registro e internacionalización hecho y en verde

--- sdi-test-38/src/com/sdi/tests/Tests/SDI2_Tests.java
+++ sdi-test-38/src/com/sdi/tests/Tests/SDI2_Tests.java
@@ -1,4 +1,5 @@
 package com.sdi.tests.Tests;
+
 import java.util.List;
 
 import org.junit.After;
@@ -10,153 +11,194 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.sdi.tests.pageobjects.PO_AltaForm;
 import com.sdi.tests.pageobjects.PO_LoginForm;
 import com.sdi.tests.utils.SeleniumUtils;
 
 //Ordenamos las pruebas por el nombre del método
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SDI2_Tests {
 
-	WebDriver driver; 
+	WebDriver driver;
 	List<WebElement> elementos = null;
-	
-	public SDI2_Tests()
-	{
+
+	public SDI2_Tests() {
 	}
 
 	@Before
-	public void run()
-	{
-		//Creamos el driver para Firefox. Recomendable usar Firefox.
+	public void run() {
 		driver = new FirefoxDriver();
-		//Vamos a la página principal de mi aplicación
-		driver.get("http://localhost:8280/sdi2-38");			
+		driver.get("http://localhost:8280/sdi2-38");
 	}
+
 	@After
-	public void end()
-	{
-		//Cerramos el navegador
+	public void end() {
 		driver.quit();
 	}
 
-	//PRUEBAS
-	
-	//	1.	[RegVal] Registro de Usuario con datos válidos.
-	@Test	
-    public void t01_RegVal() {
+	// PRUEBAS
+
+	// 1. [RegVal] Registro de Usuario con datos válidos.
+
+	@Test
+	public void t01_RegVal() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
+				"form-cabecera:linkRegistrarse");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		new PO_AltaForm().rellenaFormulario(driver, "testSDI", "testSDI",
+				"testSDI", "testSDI@mail.es", "testSDI", "testSDI");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "login-form", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Inicie sesión");
+	}
+
+	// 2. [RegInval] Registro de Usuario con datos inválidos (contraseñas //
+	// diferentes).
+
+	@Test
+	public void t02_RegInval() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
+				"form-cabecera:linkRegistrarse");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		new PO_AltaForm().rellenaFormulario(driver, "testSDIInval",
+				"testSDIInval", "testSDIInval", "testSDIInval@mail.es", "no",
+				"testSDIInval");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Registre una nueva cuenta");
+
+	}
+
+	// 3. [IdVal] Identificación de Usuario registrado con datos válidos.
+
+	@Test
+	public void t03_IdVal() {
 		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
 		new PO_LoginForm().rellenaFormulario(driver, "usuario1", "usuario1");
 		SeleniumUtils.EsperaCargaPagina(driver, "id", "bienvenido", 10);
 		SeleniumUtils.textoPresentePagina(driver, "Fernando");
-    
-    }
-	//	2.	[RegInval] Registro de Usuario con datos inválidos (contraseñas diferentes).
-    @Test
-    public void t02_RegInval() {
-    	SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+	}
+
+	// 4. [IdInval] Identificación de usuario registrado con datos inválidos.
+
+	@Test
+	public void t04_IdInval() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
 		new PO_LoginForm().rellenaFormulario(driver, "usuario1", "incorrecto");
 		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-		SeleniumUtils.textoPresentePagina(driver, "La contraseña no es correcta");
-    }
-	//	3.	[IdVal] Identificación de Usuario registrado con datos válidos.
-   /* @Test
-    public void t03_IdVal() {
-    
-    }
-	//	4.	[IdInval] Identificación de usuario registrado con datos inválidos.
-    @Test
-    public void t04_IdInval() {
-    
-    }
-	//	5.	[AccInval] Intento de acceso con URL desde un usuario no público (no identificado). Intento de acceso a vistas de acceso privado. 
-    @Test
-    public void t05_AccInval() {
-    
-    }
-	//	6.	[RegViajeVal] Registro de un viaje nuevo con datos válidos.
-    @Test
-    public void t06_RegViajeVal() {
-    
-    }
-	//	7.	[RegViajeInVal] Registro de un viaje nuevo con datos inválidos.
-    @Test
-    public void t07_RegViajeInVal() {
-    
-    }
-	//	8.	[EditViajeVal] Edición de viaje existente con datos válidos.
-    @Test
-    public void t08_EditViajeVal() {
-    
-    }
-	//	9.	[EditViajeInVal] Edición de viaje existente con datos inválidos.
-    @Test
-    public void t09_EditViajeInVal() {
-    
-    }
-	//	10.	[CancelViajeVal] Cancelación de un viaje existente por un promotor.
-    @Test
-    public void t10_CancelViajeVal() {
-    
-    }
-	//	11.	[CancelMulViajeVal] Cancelación de múltiples viajes existentes por un promotor.
-    @Test
-    public void t11_CancelMulViajeVal() {
-    
-    }
-	//	12.	[Ins1ViajeAceptVal] Inscribir en un viaje un solo usuario y ser admitido por el promotor.
-    @Test
-    public void t12_Ins1ViajeAceptVal() {
-    
-    }
-	//	13.	[Ins2ViajeAceptVal] Inscribir en un viaje dos usuarios y ser admitidos los dos por el promotor.
-    @Test
-    public void t13_Ins2ViajeAceptVal() {
-    
-    }
-	//	14.	[Ins3ViajeAceptInval] Inscribir en un viaje (2 plazas máximo) dos usuarios y ser admitidos los dos y que un tercero intente inscribirse en ese mismo viaje pero ya no pueda por falta de plazas.
-    @Test
-    public void t14_Ins3ViajeAceptInval() {
-    
-    }
-	//	15.	[CancelNoPromotorVal] Un usuario no promotor Cancela plaza.
-    @Test
-    public void t15_CancelNoPromotorVal() {
-    
-    }
-	//	16.	[Rech1ViajeVal] Inscribir en un viaje un usuario que será admitido y después rechazarlo por el promotor.
-    @Test
-    public void t16_Rech1ViajeVal() {
-    
-    }
-	//	17.	[i18N1] Cambio del idioma por defecto a un segundo idioma. (Probar algunas vistas)
-    @Test
-    public void t17_i18N1() {
-    
-    }
-	//	18.	[i18N2] Cambio del idioma por defecto a un segundo idioma y vuelta al idioma por defecto. (Probar algunas vistas)
-    @Test
-    public void t18_i18N2() {
-    
-    }
-	//	19.	[OpFiltrado] Prueba para el filtrado opcional.
-    @Test
-    public void t19_OpFiltrado() {
-    
-    }
-	//	20.	[OpOrden] Prueba para la ordenación opcional.
-    @Test
-    public void t20_OpOrden() {
-    
-    }
-	//	21.	[OpPag] Prueba para la paginación opcional.
-    @Test
-    public void t21_OpPag() {
-    
-    }
-	//	22.	[OpMante] Prueba del mantenimiento programado opcional.
+		SeleniumUtils.textoPresentePagina(driver,
+				"La contraseña no es correcta");
+		new PO_LoginForm().rellenaFormulario(driver, "noExisto", "usuario1");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "El usuario no existe");
+	}
 
-    @Test
-    public void t22_OpMante() {
-    
-    }*/
+	// 5. [AccInval] Intento de acceso con URL desde un usuario no público
+	// (no // identificado). Intento de acceso a vistas de acceso privado.
+	@Test
+	public void t05_AccInval() {
+		driver.get("http://localhost:8280/sdi2-38/infoCompleta.xhtml");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver,
+				"Formulario de inicio de sesión");
+	}
+
+	// 6. [RegViajeVal] Registro de un viaje nuevo con datos válidos.
+	/*
+	 * @Test public void t06_RegViajeVal() {
+	 * 
+	 * } // 7. [RegViajeInVal] Registro de un viaje nuevo con datos inválidos.
+	 * 
+	 * @Test public void t07_RegViajeInVal() {
+	 * 
+	 * } // 8. [EditViajeVal] Edición de viaje existente con datos válidos.
+	 * 
+	 * @Test public void t08_EditViajeVal() {
+	 * 
+	 * } // 9. [EditViajeInVal] Edición de viaje existente con datos inválidos.
+	 * 
+	 * @Test public void t09_EditViajeInVal() {
+	 * 
+	 * } // 10. [CancelViajeVal] Cancelación de un viaje existente por un
+	 * promotor.
+	 * 
+	 * @Test public void t10_CancelViajeVal() {
+	 * 
+	 * } // 11. [CancelMulViajeVal] Cancelación de múltiples viajes existentes
+	 * por un promotor.
+	 * 
+	 * @Test public void t11_CancelMulViajeVal() {
+	 * 
+	 * } // 12. [Ins1ViajeAceptVal] Inscribir en un viaje un solo usuario y ser
+	 * admitido por el promotor.
+	 * 
+	 * @Test public void t12_Ins1ViajeAceptVal() {
+	 * 
+	 * } // 13. [Ins2ViajeAceptVal] Inscribir en un viaje dos usuarios y ser
+	 * admitidos los dos por el promotor.
+	 * 
+	 * @Test public void t13_Ins2ViajeAceptVal() {
+	 * 
+	 * } // 14. [Ins3ViajeAceptInval] Inscribir en un viaje (2 plazas máximo)
+	 * dos usuarios y ser admitidos los dos y que un tercero intente inscribirse
+	 * en ese mismo viaje pero ya no pueda por falta de plazas.
+	 * 
+	 * @Test public void t14_Ins3ViajeAceptInval() {
+	 * 
+	 * } // 15. [CancelNoPromotorVal] Un usuario no promotor Cancela plaza.
+	 * 
+	 * @Test public void t15_CancelNoPromotorVal() {
+	 * 
+	 * } // 16. [Rech1ViajeVal] Inscribir en un viaje un usuario que será
+	 * admitido y después rechazarlo por el promotor.
+	 * 
+	 * @Test public void t16_Rech1ViajeVal() {
+	 * 
+	 * }
+	 */
+	// 17. [i18N1] Cambio del idioma por defecto a un segundo idioma.
+	// (Probar algunas vistas
+	@Test
+	public void t17_i18N1() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Inicie sesión");
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:idioma",
+				"form-cabecera:linkingles");
+		driver.get("http://localhost:8280/sdi2-38/registro.xhtml");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Register a new account");
+	}
+
+	// 18. [i18N2] Cambio del idioma por defecto a un segundo idioma y
+	// vuelta al idioma por defecto. (Probar algunas vistas)
+	@Test
+	public void t18_i18N2() {
+		t17_i18N1();
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:idioma",
+				"form-cabecera:linkespa");
+		driver.get("http://localhost:8280/sdi2-38/login.xhtml");
+		SeleniumUtils.textoPresentePagina(driver,
+				"Formulario de inicio de sesión");
+		driver.get("http://localhost:8280/sdi2-38/registro.xhtml");
+		SeleniumUtils.textoPresentePagina(driver, "Registre una nueva cuenta");
+	}
+	/*
+	 * // 19. [OpFiltrado] Prueba para el filtrado opcional.
+	 * 
+	 * @Test public void t19_OpFiltrado() {
+	 * 
+	 * } // 20. [OpOrden] Prueba para la ordenación opcional.
+	 * 
+	 * @Test public void t20_OpOrden() {
+	 * 
+	 * } // 21. [OpPag] Prueba para la paginación opcional.
+	 * 
+	 * @Test public void t21_OpPag() {
+	 * 
+	 * } // 22. [OpMante] Prueba del mantenimiento programado opcional.
+	 * 
+	 * @Test public void t22_OpMante() {
+	 * 
+	 * }
+	 */
 }
--- sdi-test-38/src/com/sdi/tests/pageobjects/PO_AltaForm.java
+++ sdi-test-38/src/com/sdi/tests/pageobjects/PO_AltaForm.java
@@ -1,36 +1,42 @@
 package com.sdi.tests.pageobjects;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
 public class PO_AltaForm {
 
-	
-	
-   public void rellenaFormulario(WebDriver driver, String pnombre, String papellidos, String piduser, String pemail)
-   {
-		WebElement nombre = driver.findElement(By.id("form-principal:nombre"));
+	public void rellenaFormulario(WebDriver driver, String pId, String pnombre,
+			String papellidos, String pemail,
+			String ppass, String prepass) {
+		WebElement id = driver.findElement(By.id("registro:login"));
+		id.click();
+		id.clear();
+		id.sendKeys(pId);
+		WebElement nombre = driver.findElement(By.id("registro:nombre"));
 		nombre.click();
 		nombre.clear();
 		nombre.sendKeys(pnombre);
-		WebElement apellidos = driver.findElement(By.id("form-principal:apellidos"));
+		WebElement apellidos = driver.findElement(By.id("registro:apellidos"));
 		apellidos.click();
 		apellidos.clear();
 		apellidos.sendKeys(papellidos);
-		WebElement iduser = driver.findElement(By.id("form-principal:iduser"));
-		iduser.click();
-		iduser.clear();
-		iduser.sendKeys(piduser);
-		WebElement idcorreo = driver.findElement(By.id("form-principal:correo"));
+		WebElement idcorreo = driver.findElement(By.id("registro:email"));
 		idcorreo.click();
 		idcorreo.clear();
 		idcorreo.sendKeys(pemail);
-		//Pulsar el boton de Alta.
-		By boton = By.id("form-principal:boton");
-		driver.findElement(boton).click();	   
-   }
-	
-	
-	
+		WebElement password = driver.findElement(By.id("registro:pass"));
+		password.click();
+		password.clear();
+		password.sendKeys(ppass);
+		WebElement repassword = driver
+				.findElement(By.id("registro:repitePass"));
+		repassword.click();
+		repassword.clear();
+		repassword.sendKeys(prepass);
+		// Pulsar el boton de Alta.
+		By boton = By.id("registro:registrarse");
+		driver.findElement(boton).click();
+	}
+
 }
